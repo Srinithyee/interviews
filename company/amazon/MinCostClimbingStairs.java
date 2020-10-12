a2199820bbc82fd560bb06b4d16c9b991782d27f
Srinithyee S K <sksrinithyee@gmail.com>
2020-10-12 18:14:03 +0530

    done

--- company/amazon/MinCostClimbingStairs.java
+++ company/amazon/MinCostClimbingStairs.java
@@ -33,6 +33,7 @@ class MinCostClimbingStairs {
             dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
         }
         
+        
         return Math.min(dp[cost.length - 1], dp[cost.length -2]);
     }
 }
