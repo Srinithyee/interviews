2e3c6da3e033b72c0128e152f26794232d7f2c20
Srinithyee S K <sksrinithyee@gmail.com>
2020-10-23 18:00:41 +0530

    done

--- company/amazon/MergeKSortedLists.java
+++ company/amazon/MergeKSortedLists.java
@@ -10,41 +10,26 @@
  */
 public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists==null||lists.length==0) {
-            return null;
+            PriorityQueue <Integer> minheap = new PriorityQueue <Integer> ();
+            for (ListNode head : list)
+            {
+                   while (head ! = null)
+                   {
+                       minheap.add(head.val);
+                       head = head.next;
+                   }   
+            }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        
+        while(!minheap.isEmpty())
+        {
+            head.next = new ListNode(minheap.remove());
+            head = head.next;
+        }
+            return dummy.next;    
         }
         
-        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
-            @Override
-            public int compare(ListNode o1,ListNode o2){
-                if (o1.val<o2.val) {
-                    return -1;
-                } else if (o1.val==o2.val) {
-                    return 0;
-                } else {
-                    return 1;
-                }
-            }
-        });
-        
-        ListNode dummy = new ListNode(0);
-        ListNode tail=dummy;
-        
-        for (ListNode node:lists) {
-            if (node!=null) {
-                queue.add(node);
-            }
-        }
-
-        while (!queue.isEmpty()){
-            tail.next=queue.poll();
-            tail=tail.next;
-
-            if (tail.next!=null) {
-                queue.add(tail.next);
-            }
-        }
-
-        return dummy.next;
-    }
+       
 }
