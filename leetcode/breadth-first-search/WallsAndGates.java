ac4d24a91b1515fc3e5c8a1f9886ca2b8377c50d
Srinithyee S K <sksrinithyee@gmail.com>
2020-11-05 19:19:59 +0530

    done

--- leetcode/breadth-first-search/WallsAndGates.java
+++ leetcode/breadth-first-search/WallsAndGates.java
@@ -18,30 +18,31 @@
 
 public class Solution {
     public void wallsAndGates(int[][] rooms) {
-        //iterate through the matrix calling dfs on all indices that contain a zero
-        for(int i = 0; i < rooms.length; i++) {
-            for(int j = 0; j < rooms[0].length; j++) {
-                if(rooms[i][j] == 0) {
-                    dfs(rooms, i, j, 0);
-                }
+        
+        for (i=0;i<rooms.length;i++)
+        {
+            for(j=0;j<rooms[0].length;j++)
+            {
+                if (rooms[i][j] ==0)
+                dfs(rooms, 0,i,j);
             }
         }
+        
     }
     
-    void dfs(int[][] rooms, int i, int j, int distance) {
-        //if you have gone out of the bounds of the array or you have run into a wall/obstacle, return
-        // room[i][j] < distance also ensure that we do not overwrite any previously determined distance if it is shorter than our current distance
-        if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < distance) {
+    public boolean dfs(int rooms[][], int distance, i, j)
+    {
+        while (i< 0 || i>rooms.length || j<0 || j> rooms[0].length || rooms[i][j] < distance )
+        {
             return;
         }
         
-        //set current index's distance to distance
-        rooms[i][j] = distance;
-        
-        //recurse on all adjacent neighbors of rooms[i][j]
-        dfs(rooms, i + 1, j, distance + 1);
-        dfs(rooms, i - 1, j, distance + 1);
-        dfs(rooms, i, j + 1, distance + 1);
-        dfs(rooms, i, j - 1, distance + 1);
+        rooms[i][j]=distance;
+        dfs(rooms,distance +1, i+1,j);
+        dfs(rooms,distance +1, i-1,j);
+        dfs(rooms,distance +1, i,j-1);
+        dfs(rooms,distance +1, i,j+1);
     }
 }
+        
+       
