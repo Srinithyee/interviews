f0d9ca53186696a05418c76256f15699e61ab572
Srinithyee S K <sksrinithyee@gmail.com>
2020-11-06 17:22:08 +0530

    done

--- leetcode/depth-first-search/SameTree.java
+++ leetcode/depth-first-search/SameTree.java
@@ -13,18 +13,14 @@
  */
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
+        if (p==null && q == null)
             return true;
-        }
         
-        if(p == null && q != null || q == null && p != null) {
+        if(p == null && q!=null || p!= null && q == null)
             return false;
-        }
         
-        if(p.val != q.val) {
+        if(p.val!=q.val)
             return false;
-        }
-        
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return SameTree(p.right,q.right) && SameTree(p.left,q.left);
     }
 }
