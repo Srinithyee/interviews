56ddba7ca52466160c4b994bb2bf07dee7378a4f
Srinithyee S K <sksrinithyee@gmail.com>
2020-11-09 19:41:11 +0530

    done

--- leetcode/tree/InorderSuccessorInBST.java
+++ leetcode/tree/InorderSuccessorInBST.java
@@ -15,15 +15,16 @@ public class InorderSuccessorInBST {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         TreeNode successor = null;
         
-        while(root != null) {
-            if(p.val < root.val) {
-                successor = root;
-                root = root.left;
-            } else {
-                root = root.right;
-            }
+        if(p.val <root.val)
+        {
+            successor =root;
+            root = root.left;
         }
-        
-        return successor;
+        else
+            root= root.right;
     }
+    
+    return successor;
 }
+        
+        
