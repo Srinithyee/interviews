416ecfbc68ea7b60bec5c8309374f73abc9c0997
Srinithyee S K <sksrinithyee@gmail.com>
2020-11-10 19:41:22 +0530

    done

--- leetcode/tree/TrimABinarySearchTree.java
+++ leetcode/tree/TrimABinarySearchTree.java
@@ -13,17 +13,19 @@
  */
 class TrimABinarySearchTree {
     public TreeNode trimBST(TreeNode root, int L, int R) {
-        if(root == null) {
-            return root;
+        if (root == null)
+        {
+            return null;
         }
-        if(root.val < L) {
+        if(root.val <L)
+        {
             return trimBST(root.right, L, R);
         }
-        if(root.val > R) {
+        if(root.val > R)
+        {
             return trimBST(root.left, L, R);
         }
-        
-        root.left = trimBST(root.left, L, R);
+        root.left =trimBST(root.left, L, R);
         root.right = trimBST(root.right, L, R);
 
         return root;
