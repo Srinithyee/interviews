24d1b8365c1f86a107693aed60edaa8882282078
Srinithyee S K <sksrinithyee@gmail.com>
2020-11-10 20:06:30 +0530

    done

--- leetcode/tree/LowestCommonAncestorOfABinaryTree.java
+++ leetcode/tree/LowestCommonAncestorOfABinaryTree.java
@@ -22,17 +22,16 @@
  */
 public class LowestCommonAncestorOfABinaryTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) {
-            return root;
-        }
         
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        if(root == null || p== null || q == null)
+            return root;
+        
+        TreeNode left =lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if(left != null && right != null) {
-            return root;
-        }
+        if( left!=null && right!=null)
+                return root;
         
-        return left == null ? right : left;
+        return left == null ?right:left
     }
 }
